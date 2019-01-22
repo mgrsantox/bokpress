030619484e09725dfbff23ae38261459c2139f3a
pysantosh <pysantosh.np@gmail.com>
2019-01-22 13:04:06 +0545

    abou tpage

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name="base.html")),
+    path('about/', TemplateView.as_view(template_name="about.html")),
+
 ]
