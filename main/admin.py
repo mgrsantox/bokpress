b6e192eb5bd56480c27b15ed21c847a0e791c862
pysantosh <pysantosh.np@gmail.com>
2019-01-23 16:38:58 +0545

    modeltag changed

--- main/admin.py
+++ main/admin.py
@@ -10,6 +10,7 @@ class ProductAdmin(admin.ModelAdmin):
     list_editable = ('in_stock', )
     search_fields = ('name',)
     prepopulated_fields = {"slug": ("name",)}
+    autocomplete_fields = ('tags',)
 
 
 admin.site.register(models.Product, ProductAdmin)
--- main/models.py
+++ main/models.py
@@ -3,6 +3,25 @@ from django.db import models
 # Create your models here.
 
 
+class ProductTagManager(models.Manager):
+    def get_by_natural_key(self, slug):
+        return self.get(slug=slug)
+
+
+class ProductTag(models.Model):
+    name = models.CharField(max_length=40)
+    slug = models.SlugField(max_length=50)
+    description = models.TextField(blank=True)
+    active = models.BooleanField(default=True)
+    objects = ProductTagManager()
+
+    def __str__(self):
+        return self.name
+
+    def natural_key(self):
+        return (self.slug,)
+
+
 class ActiveManager(models.Manager):
     def active(self):
         return self.filter(active=True)
@@ -16,6 +35,7 @@ class Product(models.Model):
     active = models.BooleanField(default=True)
     in_stock = models.BooleanField(default=True)
     date_updated = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField(ProductTag, blank=True)
     objects = ActiveManager()
 
     def __str__(self):
@@ -27,11 +47,3 @@ class ProductImage(models.Model):
 
     image = models.ImageField(upload_to='product_images')
     thumbnail = models.ImageField(upload_to='product_thumbnails', null=True)
-
-
-class ProductTag(models.Model):
-    product = models.ManyToManyField(Product, blank=True)
-    name = models.CharField(max_length=40)
-    slug = models.SlugField(max_length=50)
-    description = models.TextField(blank=True)
-    active = models.BooleanField(default=True)
